905372e0a1c04c888d88587398089f723241bc68
Pastel <pastel.dev.gt@gmail.com>
2022-06-21 16:59:06 -0600

    Add static folder

---
+++ public/main.js
@@ -0,0 +1,1 @@
+console.log('Hello world from Heroku and Javascript')
--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 
-app.get('/', (_req, res) => res.send('Hola a todos desde Heroku'))
+// app.get('/', (_req, res) => res.send('Hola a todos desde Heroku'))
+app.use(express.static('public'))
 
 export default app
